585b4b5aa60107f39e3834adf05876e3fb03d5bd
Mason Heuberger <110648371+Azutech7@users.noreply.github.com>
2026-07-18 03:42:34 +0000

    Moved dnscrypt-proxy over to a name that actuall matches
    Origonal file repurposed for Unbound dnssec

---
+++ modules/aspects/common.network.dnscrypt.nix
@@ -0,0 +1,30 @@
+{ inputs, den, pkgs, lib, host, ... }: {
+
+imports = [ inputs.den.flakeModule ];
+
+	den.aspects.common.provides.network.provides.dnscrypt = {
+		nixos = { ... }: {
+
+			services.dnscrypt-proxy = {
+				  enable = true;
+				  settings = {
+				  
+				    server_names = [ "cloudflare" "quad9-dnscrypt-main" ];
+				    
+				    listen_addresses = [ "127.0.0.1:53" ];
+				    require_dnssec = true;
+				    require_nolog = true;
+				  };
+				};
+
+			networking = {
+				nameservers = [ "127.0.0.1" ];
+				networkmanager = {
+					enable = true;
+					dns = "none";
+				};
+			};
+
+		};
+	};
+}
