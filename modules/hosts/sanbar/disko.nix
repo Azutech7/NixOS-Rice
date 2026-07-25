fa161c47759259dd0df8fee4d82bdbbb2904741c
Mason Heuberger <110648371+Azutech7@users.noreply.github.com>
2026-07-25 23:11:59 +0000

    dwadwhakjda

--- modules/hosts/sanbar/disko.nix
+++ modules/hosts/sanbar/disko.nix
@@ -1,43 +1,45 @@
-flake.nixosConfigurations.sanbar = {
-    # This directly forces the layout straight into the NixOS evaluation 
-    # without running it through the den aspect scraper or module loops
-    config.disko.devices.disk.sanbar_disko_disk = {
-        type = "disk";
-        device = "/dev/disk/by-id/wwn-500a0751e9aba137";
-        content = {
-            type = "gpt";
-            partitions = {
-                ESP = {
-                    name = "ESP";
-                    priority = 1;
-                    size = "1G";
-                    type = "EF00";
-                    content = {
-                        type = "filesystem";
-                        format = "vfat";
-                        mountpoint = "/boot";
-                        mountOptions = [ "umask=0077" ];
+{
+    flake.nixosConfigurations.sanbar = {
+        # This directly forces the layout straight into the NixOS evaluation 
+        # without running it through the den aspect scraper or module loops
+        config.disko.devices.disk.sanbar_disko_disk = {
+            type = "disk";
+            device = "/dev/disk/by-id/wwn-500a0751e9aba137";
+            content = {
+                type = "gpt";
+                partitions = {
+                    ESP = {
+                        name = "ESP";
+                        priority = 1;
+                        size = "1G";
+                        type = "EF00";
+                        content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                            mountOptions = [ "umask=0077" ];
+                        };
                     };
-                };
-                sanbar_disko_part = {
-                    size = "100%";
-                    content = {
-                        type = "btrfs";
-                        extraArgs = [ "-f" ]; 
-                        mountpoint = "/partition-root";
-                        subvolumes = {
-                            "/rootfs" = { mountpoint = "/"; };
-                            "/home" = {
-                                mountOptions = [ "compress=zstd" ];
-                                mountpoint = "/home";
-                            };
-                            "/nix" = {
-                                mountOptions = [ "compress=zstd" "noatime" ];
-                                mountpoint = "/nix";
-                            };
-                            "/swap" = {
-                                mountpoint = "/.swapvol";
-                                swap = { swapfile.size = "8G"; };
+                    sanbar_disko_part = {
+                        size = "100%";
+                        content = {
+                            type = "btrfs";
+                            extraArgs = [ "-f" ]; 
+                            mountpoint = "/partition-root";
+                            subvolumes = {
+                                "/rootfs" = { mountpoint = "/"; };
+                                "/home" = {
+                                    mountOptions = [ "compress=zstd" ];
+                                    mountpoint = "/home";
+                                };
+                                "/nix" = {
+                                    mountOptions = [ "compress=zstd" "noatime" ];
+                                    mountpoint = "/nix";
+                                };
+                                "/swap" = {
+                                    mountpoint = "/.swapvol";
+                                    swap = { swapfile.size = "8G"; };
+                                };
                             };
                         };
                     };
@@ -45,4 +47,4 @@ flake.nixosConfigurations.sanbar = {
             };
         };
     };
-};
+}
